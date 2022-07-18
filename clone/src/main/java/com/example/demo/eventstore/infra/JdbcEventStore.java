46728d2f2246acf5a57216266ecb5a5c937199ce
devsong26 <devsong26@gmail.com>
2022-07-18 20:37:21 +0900

    clone eventstore.infra package

---
+++ clone/src/main/java/com/example/demo/eventstore/infra/JdbcEventStore.java
@@ -0,0 +1,66 @@
+package com.example.demo.eventstore.infra;
+
+import com.example.demo.eventstore.api.EventEntry;
+import com.example.demo.eventstore.api.EventStore;
+import com.example.demo.eventstore.api.PayloadConvertException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Component
+public class JdbcEventStore implements EventStore {
+    private final ObjectMapper objectMapper;
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcEventStore(ObjectMapper objectMapper, JdbcTemplate jdbcTemplate) {
+        this.objectMapper = objectMapper;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void save(Object event){
+        EventEntry entry = new EventEntry(event.getClass().getName(),
+                MediaType.APPLICATION_JSON_VALUE, toJson(event));
+        jdbcTemplate.update(
+                "insert into evententry " +
+                "(type, content_type, payload, timestamp) " +
+                "values (?, ?, ?, ?)",
+                ps -> {
+                    ps.setString(1, entry.getType());
+                    ps.setString(2, entry.getContentType());
+                    ps.setString(3, entry.getPayload());
+                    ps.setTimestamp(4, new Timestamp(entry.getTimestamp()));
+                });
+    }
+
+    private String toJson(Object event) {
+        try {
+            return objectMapper.writeValueAsString(event);
+        } catch (JsonProcessingException e) {
+            throw new PayloadConvertException(e);
+        }
+    }
+
+    @Override
+    public List<EventEntry> get(long offset, long limit){
+        return jdbcTemplate.query(
+                "select * from evententry order by id asc limit ?, ?",
+                ps -> {
+                    ps.setLong(1, offset);
+                    ps.setLong(2, limit);
+                },
+                (rs, rowNum) -> new EventEntry(
+                        rs.getLong("id"),
+                        rs.getString("type"),
+                        rs.getString("content_type"),
+                        rs.getString("payload"),
+                        rs.getTimestamp("timestamp").getTime()
+                ));
+    }
+
+}
