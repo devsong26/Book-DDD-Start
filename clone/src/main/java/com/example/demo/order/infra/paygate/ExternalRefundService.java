2af12353ddf7616035244718f47661e94e34a6b0
devsong26 <devsong26@gmail.com>
2022-07-24 23:20:11 +0900

    clone order.infra.paygate package

---
+++ clone/src/main/java/com/example/demo/order/infra/paygate/ExternalRefundService.java
@@ -0,0 +1,18 @@
+package com.example.demo.order.infra.paygate;
+
+import com.example.demo.order.command.application.RefundService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExternalRefundService implements RefundService {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void refund(String orderNumber) {
+        logger.info("refund order[{}]", orderNumber);
+    }
+
+}
