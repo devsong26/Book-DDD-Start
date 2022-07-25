0f09385bd1d63132c853aa047606d52220d59a71
devsong26 <devsong26@gmail.com>
2022-07-25 23:12:20 +0900

    clone order.query.application package

--- clone/src/main/java/com/example/demo/order/query/application/OrderDetailService.java
+++ clone/src/main/java/com/example/demo/order/query/application/OrderDetailService.java
@@ -1,4 +1,40 @@
 package com.example.demo.order.query.application;
 
+import com.example.demo.catalog.query.product.ProductData;
+import com.example.demo.catalog.query.product.ProductQueryService;
+import com.example.demo.order.command.domain.Order;
+import com.example.demo.order.command.domain.OrderNo;
+import com.example.demo.order.command.domain.OrderRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Component
 public class OrderDetailService {
+    private final OrderRepository orderRepository;
+    private final ProductQueryService productQueryService;
+
+    public OrderDetailService(OrderRepository orderRepository,
+                              ProductQueryService productQueryService) {
+        this.orderRepository = orderRepository;
+        this.productQueryService = productQueryService;
+    }
+
+    @Transactional
+    public Optional<OrderDetail> getOrderDetail(String orderNumber){
+        Optional<Order> orderOpt = orderRepository.findById(new OrderNo(orderNumber));
+        return orderOpt.map(order -> {
+            List<OrderLineDetail> orderLines = order.getOrderLines().stream()
+                    .map(orderLine -> {
+                        Optional<ProductData> productOpt =
+                                productQueryService.getProduct(orderLine.getProductId().getId());
+                        return new OrderLineDetail(orderLine, productOpt.orElse(null));
+                    }).collect(Collectors.toList());
+            return new OrderDetail(order, orderLines);
+        });
+    }
+
 }
--- clone/src/main/java/com/example/demo/order/query/application/OrderViewListService.java
+++ clone/src/main/java/com/example/demo/order/query/application/OrderViewListService.java
@@ -1,10 +1,25 @@
 package com.example.demo.order.query.application;
 
+import com.example.demo.order.query.dao.OrderSummaryDao;
 import com.example.demo.order.query.dto.OrderSummary;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderViewListService {
-    public Page<OrderSummary> getList(ListRequest listRequest) {
-        return null;
+    private final OrderSummaryDao orderSummaryDao;
+
+    public OrderViewListService(OrderSummaryDao orderSummaryDao) {
+        this.orderSummaryDao = orderSummaryDao;
+    }
+
+    public Page<OrderSummary> getList(ListRequest listReq) {
+        PageRequest pageable = PageRequest.of(
+                listReq.getPage(),
+                listReq.getSize(),
+                Sort.by(Sort.Direction.DESC, "number"));
+        return orderSummaryDao.findAll(pageable);
     }
 }
