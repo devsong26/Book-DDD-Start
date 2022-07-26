c5423582aa1f99bc07126a0cea430d2bb603723f
devsong26 <devsong26@gmail.com>
2022-07-26 21:20:05 +0900

    clone order.query.dto package

--- clone/src/main/java/com/example/demo/order/query/dto/OrderSummary.java
+++ clone/src/main/java/com/example/demo/order/query/dto/OrderSummary.java
@@ -1,4 +1,104 @@
 package com.example.demo.order.query.dto;
 
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
+import org.hibernate.annotations.Synchronize;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Immutable
+@Subselect(
+        """
+        SELECT
+            o.order_number as number
+            , o.version
+            , o.orderer_id,
+            , o.orderer_name
+            , o.total_amounts
+            , o.receiver_name
+            , o.state
+            , o.order_state
+            , p.product_id
+            , p.name as product_name
+        FROM purchase_order o
+        INNER JOIN order_line ol ON o.order_number = ol.order_number
+        CROSS JOIN product p
+        WHERE ol.line_idx = 0
+            AND ol.product_id = p.product_id
+        """)
+@Synchronize({"purchase_order", "order_line", "product"})
 public class OrderSummary {
+    @Id
+    private String number;
+
+    private long version;
+
+    @Column(name = "orderer_id")
+    private String ordererId;
+
+    @Column(name = "orderer_name")
+    private String ordererName;
+
+    @Column(name = "total_amounts")
+    private int totalAmounts;
+
+    @Column(name = "receiver_name")
+    private String receiverName;
+
+    private String state;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "product_id")
+    private String productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    protected OrderSummary(){}
+
+    public String getNumber() {
+        return number;
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    public String getOrdererId() {
+        return ordererId;
+    }
+
+    public String getOrdererName() {
+        return ordererName;
+    }
+
+    public int getTotalAmounts() {
+        return totalAmounts;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
 }
