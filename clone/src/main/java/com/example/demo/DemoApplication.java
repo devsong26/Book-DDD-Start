5e00d888814be41f4737d52d46b8f0de3207a5a9
devsong26 <devsong26@gmail.com>
2022-07-29 22:59:02 +0900

    clone CONFIG FILES

--- clone/src/main/java/com/example/demo/DemoApplication.java
+++ clone/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.common.jpa.RangeableRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@EnableJpaRepositories(repositoryBaseClass = RangeableRepositoryImpl.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
